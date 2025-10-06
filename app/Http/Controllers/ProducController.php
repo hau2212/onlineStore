3b5149a1bd16809dcf29b4ceecf5e6172ce93c85
hau <hau@gmail.com>
2025-10-06 10:07:15 +0700

    oop

--- app/Http/Controllers/ProducController.php
+++ app/Http/Controllers/ProducController.php
@@ -2,28 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProducController extends Controller
 {
-    public static $product = [
-        ["id"=>'1' , "name" => "TV" , "description" => "Best TV" , "image" => "game.png" , "price" => "1000"],
-        ["id"=>'1' , "name" => "TV" , "description" => "Best TV" , "image" => "game.png" , "price" => "1000"],
-        ["id"=>'1' , "name" => "TV" , "description" => "Best TV" , "image" => "game.png" , "price" => "1000"],
-        ["id"=>'1' , "name" => "TV" , "description" => "Best TV" , "image" => "game.png" , "price" => "1000"]
-    ];
+#    public static $product = [
+#        ["id"=>'1' , "name" => "TV" , "description" => "Best TV" , "image" => "game.png" , "price" => "1000"],
+#        ["id"=>'2' , "name" => "TV" , "description" => "Best TV" , "image" => "safe.png" , "price" => "1000"],
+#        ["id"=>'3' , "name" => "TV" , "description" => "Best TV" , "image" => "submarine.png" , "price" => "1000"],
+#        ["id"=>'4' , "name" => "TV" , "description" => "Best TV" , "image" => "fukada.png" , "price" => "1000"]
+#    ];
     public function index(){
         $viewData = [];
         $viewData["title"] = "Product - Online Store";
         $viewData["subtitle"] = "list of products";
-        $viewData["product"] = ProducController::$product;
+        $viewData["product"] = Product::all();
         return view("product.index") -> with("viewData" , $viewData); 
     }
     public function show($id){
         $viewData = [];
-        $product = ProducController::$product[$id - 1];
-        $viewData["title"] = $product['name'] . " - Online Store";
-        $viewData["subtitle"] = $product["name"] . " - Product Information";
+        $product = Product::findOrFail($id);
+        $viewData["title"] = $product -> getName(). "online Store";
+        $viewData["subtitle"] = $product -> getName() . " - Product Information";
         $viewData["product"] = $product;
         return view('product.show') -> with("viewData" , $viewData);
     }
---
+++ app/Models/Product.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    #use HasFactory;
+##################################____GET____#####################################################
+    public function getID(){
+        return $this -> attributes['id'];
+    }
+    public function getName(){
+        return $this -> attributes['name'];
+    }
+    public function getDescription(){
+        return $this -> attributes['description'];
+    }
+    public function getImage(){
+        return $this -> attributes['image'];
+    }
+    public function getPrice(){
+        return $this -> attributes['price'];
+    }
+    public function getCreatedAt(){
+        return $this -> attributes['created_at'];
+    }
+    public function getUpdateAt(){
+        return $this -> attributes['update_at'];
+    }
+##################################___SET___#####################################################
+    public function setID($id){
+        $this -> attributes['id'] = $id;
+    }
+    public function setName($name){
+         $this -> attributes['name'] = $name;
+    }
+    public function setDescription($description){
+        $this -> attributes['description'] = $description;
+    }
+    public function setImage($image){
+        $this -> attributes['image'] = $image;
+    }
+    public function setCreatedAt($created_at){
+        $this -> attributes['created_at'] = $created_at;
+    }
+    public function setUpdateAt($update_at){
+        $this -> attributes['update_at'] = $update_at;
+    }
+
+
+
+}
