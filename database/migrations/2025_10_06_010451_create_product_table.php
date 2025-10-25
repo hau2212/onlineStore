235a3f043be8c39ef3661decf67aece0e6a188be
thanh hau <mr.hau221222@gmail.com>
2025-10-25 22:44:50 +0700

    d

--- database/migrations/2025_10_06_010451_create_product_table.php
+++ database/migrations/2025_10_06_010451_create_product_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        #nho chinh lai products
         Schema::create('product', function (Blueprint $table) {
             $table->id();
             $table->string("name");
